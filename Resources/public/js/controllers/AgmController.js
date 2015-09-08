066b45eb7feffe779e23c4ba87e3fdf554236655
= <shsuserh@gmail.com>
2015-09-08 15:25:15 +0400

    Change

--- Resources/public/js/controllers/AgmController.js
+++ Resources/public/js/controllers/AgmController.js
@@ -1,4 +1,9 @@
 app.controller('AgmController',function($scope){
-
-
+    $scope.noEnter = function(e){
+        var code = e.keyCode || e.which;
+        if (code == 13) {
+            e.preventDefault();
+            return false;
+        }
+    };
 });
